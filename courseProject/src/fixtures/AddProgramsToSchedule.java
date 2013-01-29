da254adc4a7b7fa5cf1df5e049aaa7107361aa86
Wisang Eom <wseom7@gmail.com>
2013-01-29 09:52:15 +0900

    first fixture

---
+++ courseProject/src/fixtures/AddProgramsToSchedule.java
@@ -0,0 +1,32 @@
+package fixtures;
+
+public class AddProgramsToSchedule {
+
+	public void setName(String programName) {
+
+	}
+
+	public void setEpisode(String episode) {
+
+	}
+
+	public void setDate(String episode) {
+
+	}
+
+	public void setChannel(int channel) {
+
+	}
+
+	public void setStartTime(String time) {
+
+	}
+
+	public void setMinutes(int duration) {
+
+	}
+	
+	public String id() {
+		return "test";
+	}
+}
